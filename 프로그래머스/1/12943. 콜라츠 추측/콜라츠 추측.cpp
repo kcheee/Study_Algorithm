574435483a319910e954e8621eeca81535af6641
kcheee <86779278+kcheee@users.noreply.github.com>
2023-12-09 01:52:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+    // 짝수면 2로 나눔
+    // 홀수면 3곱하고 1더함
+    // 1이될때까지 반복
+    // 몇번이나 반복해야 하는지 체크.
+
+int solution(int n) {
+    int answer = 0;
+    long long num =n;
+    while(1){
+        if(num==1) break;
+        
+        //짝수라면
+        if(num%2==0)
+            num=num/2;  
+        else        
+            num = (num*3) +1;
+                              
+        answer++;
+        if(answer>=500) return -1;
+    }
+    cout<<answer;
+    return answer;
+}
