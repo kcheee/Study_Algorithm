dcad969bd43fa07c89135c1c88894add4f9d1253
강찬희 <86779278+kcheee@users.noreply.github.com>
2024-02-18 23:05:55 +0900

    [level 2] Title: 피보나치 수, Time: 0.73 ms, Memory: 4.39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// 시간 초과
+int fibonachi(int a)
+{
+    if(a==0)
+        return 0;
+    if(a==1)
+        return 1;
+    return fibonachi(a-1)+fibonachi(a-2);
+}
+int solution(int n) {
+    
+    // f(0) = 0
+    // f(1) = 1
+    // f(n) = f(n-1)+f(n-2)
+    // dp 문제
+    //
+    long long dp[100001];
+    dp[0] =0;
+    dp[1] =1;
+    for(int i=2;i<sizeof(dp)/sizeof(long long);i++)
+    {
+        dp[i]=(dp[i-1]+dp[i-2])%1234567;
+    }
+    long long answer = 0;
+    answer = dp[n];
+    return answer;
+}
