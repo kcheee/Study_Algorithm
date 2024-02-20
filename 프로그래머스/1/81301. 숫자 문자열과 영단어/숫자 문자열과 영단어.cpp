e58cbf4d6d9f9f248704aa601a0b90e31f7dcbdd
강찬희 <86779278+kcheee@users.noreply.github.com>
2024-02-20 17:56:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 3.61 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -7,14 +7,6 @@ using namespace std;
 
 int solution(string s) {
     
-    // string 값으로 영어와 숫자가 섞인 값이 들어옴
-    // 아스키코드로 설정
-    // STL map을 사용해서 key value 값으로 설정해서 만든다
-    
-    
-    // map을 사용해서 영단어로 된 key를 value 저장
-    // 만약 아스키코드로 97 122안의 값이 나온다면 숫자가 나올때까지 s_answer에 저장
-    // 숫자가 나온다면 s_answer의값을 Key에서 찾고 value값을 답에 더한다.
     map<string,string> m_number;
     
     m_number.insert({"zero","0"});    m_number.insert({"one","1"});    m_number.insert({"two","2"});
@@ -39,7 +31,6 @@ int solution(string s) {
 	        }
         }
         // 숫자이고 전에 영어가 나왔다면 map에서 key값을 찾는다.
-        // 바로 영어가 나오는것도 찾아야함.
         else if(int(s[i-1])>96 && 123>int(s[i-1]))
         {          
            ans += m_number[s_answer]+s[i];         
