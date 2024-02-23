79dad1369c3677e223926e58fc447453a5b2dde0
강찬희 <86779278+kcheee@users.noreply.github.com>
2024-02-23 23:04:07 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -19,14 +19,14 @@ int gcd(int a, int b) {
 
 // 최소공배수 계산
 int lcm(int a, int b) {
-    return a / gcd(a, b) * b; // 오버플로우를 방지하기 위해 순서 주의
+    return a *b / gcd(a, b); // 오버플로우를 방지하기 위해 순서 주의
 }
 
 int solution(vector<int> arr) {
     int result = arr[0]; // 배열의 첫 번째 요소로 초기화
 
     for (int i = 1; i < arr.size(); i++) {
-        result = lcm(result, arr[i]); // 현재까지의 최소공배수와 다음 요소의 최소공배수를 계산
+        result = lcm(result, arr[i]); 
     }
 
     return result;
