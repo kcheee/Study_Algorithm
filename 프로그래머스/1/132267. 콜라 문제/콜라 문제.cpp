7f6032e0bd4125239b6cd7cab9bea6044e701712
강찬희 <86779278+kcheee@users.noreply.github.com>
2024-02-23 19:48:28 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -9,12 +9,6 @@ int solution(int a, int b, int n) {
     // 빈병 2개 콜라 하나
     // 빈병 a개를 주면 콜라 b병을 주는 마트에서 빈병 n개를 가져다 주면 총 몇 병을 받을 수 있는지
     
-    int answer = 0;
-    while(n/a!=0)
-    {
-        answer+= (n/a)*b;
-        n= ((n/a)*b)+(n%a);
-    }
     
-    return answer;
+    return (n - b) / (a - b) * b;
 }
