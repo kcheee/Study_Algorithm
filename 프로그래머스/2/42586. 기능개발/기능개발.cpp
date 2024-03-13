c72f0d1552ee0f0ca40547c1dc3595ad6ef60d76
강찬희 <86779278+kcheee@users.noreply.github.com>
2024-03-13 17:16:16 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -4,28 +4,17 @@
 
 using namespace std;
 
-// 배포될때 같이 배포가 되는 작업들
-// 앞에 있는 작업보다 뒤에 있는 작업이 오래 걸리면 오래걸리는 작업 앞에 있는 것 까지 카운트
-// 작업이 언제 끝나는지 카운트
-
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     int result = 0;
     int cnt =1;
     vector<int> ans;
-    // 작업이 하나일때도 체크 해줘야 함.
-   if(progresses.size()==1) 
-   {
-       int n = ((100-progresses[0]) % speeds[0])>0 ? 
-            (100-progresses[0]) / speeds[0]+1 : (100-progresses[0]) / speeds[0];
-       answer.push_back(n);
-       return answer;
-   }
+
     for(int i=0;i<progresses.size();i++)
     {
         int n = ((100-progresses[i]) % speeds[i])>0 ? 
             (100-progresses[i]) / speeds[i]+1 : (100-progresses[i]) / speeds[i];
-        // cout<<n<<" ";
+
         if(i==0) result=n;
         else
         {
@@ -41,9 +30,7 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
                 cnt++;
             }
         }
-
     }
-    // 마지막 값
     answer.push_back(cnt);
        
     return answer;
