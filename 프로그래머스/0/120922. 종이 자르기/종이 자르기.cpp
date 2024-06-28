0837b70aeab82b981ada0fabdb32e89e21dcb988
강찬희 <86779278+kcheee@users.noreply.github.com>
2024-06-29 01:08:48 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 2 3 5
+// 2 4 7
+// 2 5 9
+// 3 3
+int solution(int M, int N) {
+    int answer = M*N-1;
+    
+    return answer;
+}
